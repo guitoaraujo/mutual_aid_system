4934aa79911ae8f845f5644a1c10eb13e823cf0d
Igor Araujo <igor.araujo@fulllab.com.br>
2018-12-03 16:59:50 -0200

    implemented user custom attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    attrs = [:name, :phone, :cpf, :country, :zipcode, :mibank]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attrs)
+    devise_parameter_sanitizer.permit(:account_update, keys: attrs.push(:email))
+  end
 end
